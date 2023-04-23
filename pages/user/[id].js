9c7fbfd8fa3b4e8222c38d1010f06a566c7cf531
BayajidAlam <bayajidalam2001@gmail.com>
2023-04-23 17:31:40 +0600

    server side rendering

---
+++ pages/user/[id].js
@@ -0,0 +1,26 @@
+function SingleUser({user}) {
+  
+  return (
+    <div>
+      <h2>Single User</h2>
+      <h3>Name: {user.name}</h3>
+    </div>
+  );
+}
+
+export async function getStaticProps(ctx){
+
+  const { params } = ctx
+
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`);
+  const data = await res.json();
+
+
+  return {
+    props:{
+      user: data
+    }
+  }
+}
+
+export default SingleUser;
