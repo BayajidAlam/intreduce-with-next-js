b2e5084a8fc3fbc33f9bf0c890c097a2df758e4b
BayajidAlam <bayajidalam2001@gmail.com>
2023-04-24 11:38:33 +0600

    crud done

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,7 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
     <div>
       <h1>This is next project home page</h1>
-      <Link href="/about" legacyBehavior>
-        <a>
-          <h3>Go to about page</h3>
-        </a>
-      </Link>
-      <Link href="/about" legacyBehavior>
-        <a>
-          <h3>Go to about page</h3>
-        </a>
-      </Link>
     </div>
   );
 }
